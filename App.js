da991273740386b3af985fd463f743e15e388b9f
Kushul Soomaree <kushul@lsl.digital>
2024-04-13 18:43:07 +0400

    login/signup screen

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import LoginSignupScreen from './app/screens/LoginSignupScreen';
 
 export default function App() {
   return (
-    <WelcomeScreen />
+    <LoginSignupScreen />
   );
 }
